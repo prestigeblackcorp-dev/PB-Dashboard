24f941413ebed308515c5fd9803239a1df2cc684
Jalen Nguyen <jalennguyen@Jalens-MacBook-Air.local>
2026-06-16 21:33:43 -0500

    Revert "Near-instant cross-device updates: adaptive ~1.5s poll, re-render only on change (v132)"
    This reverts commit fafc02171df3c0e4b8be455fe8d4408228cf842d.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pb-v132';
+const CACHE = 'pb-v131';
 const ASSETS = ['./index.html', './icon.png', './obsidian.html', './driver.html', './pb-config.js'];
 
 self.addEventListener('install', function(e) {
